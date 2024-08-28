dc118c9771b98562ca1cadaa796930f27b17bb53
Jisoo <voidmule.xx@gmail.com>
2024-08-28 09:15:34 +0900

    JSLookBookViewController 상세페이지 사진  추가

--- OOTD_Team3/OOTD_Team3/ViewControllers/JSLookBookViewController.swift
+++ OOTD_Team3/OOTD_Team3/ViewControllers/JSLookBookViewController.swift
@@ -8,30 +8,25 @@
 import UIKit
 
 class JSLookBookViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let pageImageView = UIImageView()
         pageImageView.image = UIImage(named: "upload")
         pageImageView.contentMode = .scaleAspectFit
+        pageImageView.clipsToBounds = true
         //vstackView.addArrangedSubview(pageImageView)
         // Do any additional setup after loading the view.
+        
+        view.addSubview(pageImageView)
+        
+        // 오토 레이아웃 설정
+        pageImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pageImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            pageImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-    
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
-//#Preview {
-//    JSLookBookViewController()
-//}
