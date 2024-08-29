be2e8fab8af92a3da7ab4841f83031db31e91a0e
young <85962238+dardardardardar@users.noreply.github.com>
2024-08-29 16:11:20 +0900

    ootd탭 updates

---
+++ OOTD_Team3/OOTD_Team3/ViewControllers/OOTDViewController/OOTDDetailesViewController.swift
@@ -0,0 +1,136 @@
+//
+//  LoginViewController.swift
+//  OOTD_Team3
+//
+//  Created by 이다영 on 8/27/24.
+//
+
+import UIKit
+
+class OOTDDetailesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+    }
+
+    // 하트가 버튼 + 클릭 동작 확인
+    private let heartButton = UIButton(type: .system)
+    var isclicked = false
+    
+    // 구매하기 버튼
+    let buyButton = UIButton(type: .system)
+    
+    private func setupUI() {
+        // 스크롤뷰 설정
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+
+        // 컨텐츠뷰 설정
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+
+        // 로고
+        let logoImageView = UIImageView(image: UIImage(named: "White"))
+        logoImageView.contentMode = .scaleAspectFill
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImageView)
+
+        // 이미지 뷰
+        let imageView = UIImageView(image: UIImage(named: "ootdDetailesImage"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(imageView)
+        
+        // 하트 누르기 버튼
+        heartButton.setTitle("♡", for: .normal)
+        heartButton.backgroundColor = .gray
+        heartButton.setTitleColor(.white, for: .normal)
+        heartButton.layer.cornerRadius = 8
+        heartButton.translatesAutoresizingMaskIntoConstraints = false
+        heartButton.addTarget(self, action: #selector(heartbuttonTapped), for: .touchUpInside)
+
+        // 구매하기 버튼
+        buyButton.setTitle("구매하기", for: .normal)
+        buyButton.backgroundColor = .black
+        buyButton.setTitleColor(.white, for: .normal)
+        buyButton.layer.cornerRadius = 8
+        buyButton.translatesAutoresizingMaskIntoConstraints = false
+        buyButton.addAction(UIAction(title: "구매하기") { _ in }, for: .touchUpInside)
+        buyButton.addTarget(self, action: #selector(paySheetViewController), for: .touchUpInside)
+
+        // 버튼들을 담을 UIStackView 설정
+        let actionStackView = UIStackView(arrangedSubviews: [heartButton, buyButton])
+        actionStackView.axis = .horizontal
+        actionStackView.spacing = 10
+        actionStackView.distribution = .fillEqually
+        actionStackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(actionStackView)
+
+        // 레이아웃 설정
+        NSLayoutConstraint.activate([
+            // 로고 레이아웃 제약조건
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 20),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+
+            // 스크롤뷰 제약조건
+            scrollView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            // 컨텐츠뷰 제약조건
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+
+            // 이미지뷰 제약조건
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.3),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+
+            // 스택 뷰 제약조건
+            actionStackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            actionStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            actionStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            actionStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            actionStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    // 하트 클릭 동작
+    @objc private func heartbuttonTapped() {
+        if isclicked {
+            // 이미지가 보이는 상태라면 이미지를 제거
+            heartButton.setTitle("♥", for: .normal)
+        } else {
+            heartButton.setTitle("♡", for: .normal)
+        }
+        // 상태를 반전시킴
+        isclicked.toggle()
+    }
+    
+    // 구매 동작
+    @objc private func paySheetViewController() {
+        let paysheetview = PayViewController()
+        
+        if let sheet = paysheetview.sheetPresentationController {
+            sheet.detents = [.large(), .large()]
+            sheet.prefersGrabberVisible = true
+        }
+        present(paysheetview, animated: true, completion: nil)
+    }
+}
+
+#Preview {
+    OOTDDetailesViewController()
+}
+
--- OOTD_Team3/OOTD_Team3/ViewControllers/OOTDViewController/OOTDViewController.swift
+++ OOTD_Team3/OOTD_Team3/ViewControllers/OOTDViewController/OOTDViewController.swift
@@ -15,19 +15,18 @@ class OOTDViewController: UIViewController, UICollectionViewDelegate, UICollecti
         // UICollectionView의 레이아웃 설정
         let layout = UICollectionViewFlowLayout()
         
+        let spacing: CGFloat = 10
         let numberOfItemsPerRow: CGFloat = 2
-        let numberOfRows: CGFloat = 3
-        
 
         // 화면의 너비를 가져와서 셀의 크기를 동적으로 계산합니다.
-        let totalSpacing = (numberOfItemsPerRow - 1) * 10
-        let itemWidth = (UIScreen.main.bounds.width - totalSpacing - (10 * 2)) / numberOfItemsPerRow
+        let totalSpacing = (numberOfItemsPerRow + 1) * spacing
+        let itemWidth = (UIScreen.main.bounds.width - totalSpacing) / numberOfItemsPerRow
         
         // Cell의 크기와 간격 조정
         layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
-        layout.minimumLineSpacing = 5
-        layout.minimumInteritemSpacing = 5
-        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.dataSource = self
@@ -36,6 +35,7 @@ class OOTDViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
         // 재활용 가능한 custom cell 선언
         collectionView.register(MyCell.self, forCellWithReuseIdentifier: "MyCell")
+
         
         return collectionView
     }()
@@ -66,20 +66,10 @@ class OOTDViewController: UIViewController, UICollectionViewDelegate, UICollecti
         items = ItemStore.shared.items
         
         setupInterface()
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        // 네비게이션 바 숨기기
-        navigationController?.setNavigationBarHidden(true, animated: false)
+        title = "OOTD"
     }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // 다른 화면으로 이동할 때 네비게이션 바를 계속 숨기기
-        navigationController?.setNavigationBarHidden(true, animated: false)
-    }
+    
     
     func setupInterface() {
         view.addSubview(buttonStackView)
@@ -139,24 +129,35 @@ class OOTDViewController: UIViewController, UICollectionViewDelegate, UICollecti
         // 예시 이미지 설정
         cell.imageView.image = UIImage(named: item.itemImage)
         
+        //버튼 추가
+        cell.infoButton.addTarget(self, action: #selector(showDetailes), for: .touchUpInside)
         return cell
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
     }
     
+    @objc func showDetailes() {
+        let detailsVC = OOTDDetailesViewController()
+        if let navigationController = navigationController {
+            navigationController.pushViewController(detailsVC, animated: true)
+        } else {
+            present(detailsVC, animated: true, completion: nil)
+        }
+    }
 }
 
 class MyCell: UICollectionViewCell {
     let imageView: UIImageView
     private let heartImageView: UIImageView
+    let infoButton: UIButton
     private var isHeartVisible = false
     
     override init(frame: CGRect) {
         imageView = UIImageView()
         heartImageView = UIImageView(image: UIImage(systemName: "heart.fill"))
+        infoButton = UIButton(type: .system)
         
         super.init(frame: frame)
         
@@ -167,11 +168,19 @@ class MyCell: UICollectionViewCell {
         
         heartImageView.contentMode = .scaleAspectFit
         heartImageView.tintColor = .red
-        heartImageView.frame = CGRect(x: contentView.bounds.width - 30, y: 0, width: 30, height: 30)
+        heartImageView.frame = CGRect(x: contentView.bounds.width - 30, y: 150, width: 30, height: 30)
         heartImageView.isHidden = true // 처음에는 하트 이미지 숨김
         
+        infoButton.setTitle("정보", for: .normal)
+        infoButton.backgroundColor = .gray
+        infoButton.setTitleColor(.white, for: .normal)
+        infoButton.layer.cornerRadius = 5
+        infoButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        infoButton.frame = CGRect(x: 5, y: contentView.bounds.height - 30, width: 40, height: 25)
+
         contentView.addSubview(imageView)
         contentView.addSubview(heartImageView)
+        contentView.addSubview(infoButton)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         contentView.addGestureRecognizer(tapGesture)
@@ -186,7 +195,6 @@ class MyCell: UICollectionViewCell {
         heartImageView.isHidden = !isHeartVisible
     }
 }
-
 
 #Preview {
     OOTDViewController()
